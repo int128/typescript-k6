a89ccb54905a6a844a3d20f412a1e7204ea031c9
Hidetake Iwata <int128@gmail.com>
2023-03-19 10:58:58 +0900

    Remove jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
-// https://typescript-eslint.io/docs/linting/
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: true,
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
   },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
 }
