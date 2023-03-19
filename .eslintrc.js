84bbfe133b4c899eb36f844c7ee913deefbb64df
Hidetake Iwata <int128@gmail.com>
2023-03-19 11:01:20 +0900

    Initial commit (#2)
    * Initial commit
    
    * Fix
    
    * Remove jest

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+}
